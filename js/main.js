fcb87f9e16f07fa97a6e9f013b878782ffefbb8e
Jordan Coverdale <60715643+jordancov@users.noreply.github.com>
2023-08-19 18:37:22 +0100

    Made data print from api

---
+++ js/main.js
@@ -0,0 +1,9 @@
+async function getUsers() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    document.write(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
